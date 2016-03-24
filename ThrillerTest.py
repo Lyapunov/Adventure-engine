f451ea865ad0d4b887558ffddc2ca3fcd0a5bad0
Ritchie Vasko <vaskorichard@gmail.com>
2016-03-24 05:15:34 +0100

    Rewriting the test about the stone and the bird, it goes to red.

--- ThrillerTest.py
+++ ThrillerTest.py
@@ -16,12 +16,17 @@ class Game:
    def has( self, name ):
       object = self.inventory.find( name )
       return object
+   def use( self, name1, name2 ):
+      pass 
+   def is_in_world( self, name ):
+      return self.world.find( name )
 
 class ThrillerTest(unittest.TestCase):
    def setUp( self ):
-      self.game = Game( GameObject( 'room','dark room', [ GameObject( 'candle' ), GameObject('match') ] ),
+      self.game = Game( GameObject( 'room','dark room', [ GameObject( 'candle' ), GameObject('match'), GameObject('bird'), GameObject('stone') ] ),
                         GameObject( 'inventory', 'my inventory', [] ),
-                        [ GameObjectAction( GameObject('candle'), GameObject('match'), 'you light the candle with the match', GameObject('burning candle') ) ] );
+                        [ GameObjectAction( GameObject('candle'), GameObject('match'), 'light the candle with the match', GameObject('burning candle') ),
+                          GameObjectAction( GameObject('bird'), GameObject('stone'),   'hit the bird with the stone', GameObject('injured bird') ) ] );
 
    def test_look_in_room(self):
       text = self.game.world.look()
@@ -44,18 +49,14 @@ class ThrillerTest(unittest.TestCase):
       self.game.inventory.put( object )
       assert ( not self.game.inventory.take('knife') is None )
 
-   def test_use_bird_on_stone(self):
-      bird  = GameObject( 'bird' )
-      stone = GameObject( 'stone' )
-      burning_stone = GameObject( 'injured bird' )
-      actionDescription = 'you hit the bird with the stone';
-      action = GameObjectAction( bird, stone, actionDescription, burning_stone )
-      self.game.actions.append( action )
-      assert ( stone.use( bird, self.game.actions ) is None )
-      assert ( bird.name == 'bird' )
-      assert ( bird.use( stone, self.game.actions ) == actionDescription )
-      assert ( bird.name == 'injured bird' )
-      assert ( not action in self.game.actions )
+   def test_action_hit_the_bird_with_the_stone(self):
+      self.game.take( 'stone' )
+      object1 = self.game.use( 'stone', 'bird' )
+      assert ( object1 is not None )
+      assert ( self.game.is_in_world( 'bird' ) is None )
+      assert ( not self.game.is_in_world( 'injured bird' ) is None )
+      object2 = self.game.use( 'stone', 'bird' )
+      assert ( object2 is not None )
 
    def test_put_burning_candle_to_inventory_to_light_room(self):
       self.game.inventory.put( GameObject( 'burning candle' ) )
