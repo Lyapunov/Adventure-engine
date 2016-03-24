3baddf37d50eff1adf5c768d031dd2bc58e51795
Ritchie Vasko <vaskorichard@gmail.com>
2016-03-24 04:26:51 +0100

    Initial revision from yesterday.

---
+++ GameObject.py
@@ -0,0 +1,39 @@
+
+class GameObjectAction:
+   def __init__( self, main, manipulator, actionDescription, prototype ):
+      self.main              = main
+      self.manipulator       = manipulator
+      self.actionDescription = actionDescription
+      self.prototype         = prototype
+
+class GameObject:
+   def __init__( self, name = '', description = '', childObjects = []):
+      self.name = name
+      self.description = description
+      self.childObjects = childObjects
+
+   def makeEqualTo( self, other ):
+      self.name         = other.name
+      self.description  = other.description
+      self.childObjects = other.childObjects
+
+   def look( self ):
+      return self.description
+
+   def take( self, name ):
+      for child in self.childObjects:
+         if child.name == name:
+            self.childObjects.remove( child )
+            return child
+      return None 
+
+   def put( self, child ):
+      self.childObjects.append( child )
+
+   def use( self, manipulator, actions ):
+      for action in actions:
+         if action.main == self and manipulator == action.manipulator:
+            actions.remove(action)
+            self.makeEqualTo( action.prototype ) 
+            return action.actionDescription
+      return None
---
+++ ThrillerTest.py
@@ -0,0 +1,40 @@
+import unittest
+
+from GameObject import GameObject
+from GameObject import GameObjectAction
+
+class ThrillerTest(unittest.TestCase):
+   def setUp( self ):
+      self.game     = GameObject( 'room','dark room', [ GameObject( 'candle' ), GameObject('match') ] )
+      self.backpack = GameObject( 'backpack', 'my backpack', [] );
+      self.actions  = [ GameObjectAction( GameObject('candle'), GameObject('match'), 'you light the candle with the match', GameObject('burning candle') ) ]
+
+   def test_look_in_room(self):
+      text = self.game.look()
+      assert( text == 'dark room' )
+
+   def test_take_candle(self):
+      object = self.game.take('candle')
+      assert ( not object is None and object.name == 'candle' )
+
+   def test_put_knife_in_backpack(self):
+      object = GameObject( 'knife' )
+      self.backpack.put( object )
+      assert ( not self.backpack.take('knife') is None )
+
+   def test_use_bird_on_stone(self):
+      bird  = GameObject( 'bird' )
+      stone = GameObject( 'stone' )
+      burning_stone = GameObject( 'injured bird' )
+      actionDescription = 'you hit the bird with the stone';
+      action = GameObjectAction( bird, stone, actionDescription, burning_stone )
+      self.actions.append( action )
+      assert ( stone.use( bird, self.actions ) is None )
+      assert ( bird.name == 'bird' )
+      assert ( bird.use( stone, self.actions ) == actionDescription )
+      assert ( bird.name == 'injured bird' )
+      assert ( not action in self.actions )
+
+if __name__ == '__main__' :
+   unittest.main()
+
