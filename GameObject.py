790b9268df500ff1e13d570c21deddb3b0af62a9
Ritchie Vasko <vaskorichard@gmail.com>
2016-03-25 04:10:47 +0100

    Moving inventory object to GameObject object.

--- GameObject.py
+++ GameObject.py
@@ -3,7 +3,7 @@ import copy
 class Game:
    def __init__( self, world, use_actions, views ):
       self.world = world
-      self.inventory = GameObject( 'inventory', 'my inventory', [] ),
+      self.inventory = GameObject( 'inventory', 'my inventory', [] )
       self.use_actions = use_actions
       self.views = views
 
