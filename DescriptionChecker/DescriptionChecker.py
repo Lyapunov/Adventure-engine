b5e7e88f5655ede1a4206d5dd601c1a4c25922e1
Lyapunov <vaskorichard@gmail.com>
2016-09-11 16:47:18 +0200

    Adding description checker. (TextEngine already had, so just factored out.)

---
+++ DescriptionChecker/DescriptionChecker.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python2.7
+
+import json
+import os.path
+import sys
+sys.path.append("../PyLib")
+
+from GameObject import Game
+from GameObject import GameDecoder
+from GameObject import GameSyntaxChecker
+
+def main(argv):
+   if len(argv) != 2:
+      print "USAGE:", os.path.basename( argv[0] ), "<game dat file>"
+      return
+   gamefile = argv[1]
+   exit_commands = [ 'quit', 'exit' ]
+   inventory_commands = [ 'inventory' ];
+   game = None;
+   try:
+      with open(gamefile,'r') as myfile:
+         game_blueprints = myfile.read().replace('\n','')
+         game = Game( GameDecoder().decode( game_blueprints ) )
+   except IOError as e:
+      print "I/O error({0}): {1}".format(e.errno, e.strerror)
+      return
+   except Exception as e:
+      print "Could not parse game file."
+      print e
+      return
+   result = GameSyntaxChecker().check( game );
+   if result != "":
+      print "Error: ", result;
+      return
+   else:
+      print "Game description in",gamefile,"is error-free."
+
+if __name__ == "__main__":
+    main(sys.argv)
